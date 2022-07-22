182e6ac1608032c676787d479347f1641a904e7c
timarciogarcia <timarciogarcia@gmail.com>
2022-07-22 14:36:25 -0300

    inicio domain e data

---
+++ AR.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AR.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ AR.Domain/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AR.Domain
+{
+    public class Cliente
+    {
+        [Key]   
+        public Guid Id { get; set; }
+        
+        [Required(ErrorMessage ="P campo {0} é requerido !")]
+        [MaxLength(50, ErrorMessage ="O campo {0}, deve ter no máximo {1} caracteres. ")]
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+
+        public int Idade { get; set; }
+
+    }
+}
